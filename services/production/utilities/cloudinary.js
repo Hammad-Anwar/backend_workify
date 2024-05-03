bd0bab5b8116a189a62b8e521845507f6cc598bc
Hammad-Anwar <hammad_anwar@outlook.com>
2024-05-04 01:08:18 +0500

    ff

---
+++ services/production/utilities/cloudinary.js
@@ -0,0 +1,7 @@
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({ 
+    cloud_name: 'dskffiksr', 
+    api_key: '424982255271138', 
+    api_secret: 'P4Z5k3-0QouAnyVou8EA74g33Es'
+  });
